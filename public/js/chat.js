a51e4b82c336854fc575e4aea474e7b80d04fcf1
Yash Mishra <yashmishra12@hotmail.com>
2020-08-01 18:56:42 +0530

    Adding Event Acknowledgement

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,9 +1,42 @@
 const socket = io()
 
-socket.on('countUpdated',(count)=>{
-    console.log("The count has been updated",count);
+socket.on('message', message=>{
+    console.log(message);
 })
 
-document.querySelector('#incrementButton').addEventListener('click', ()=>{
-    socket.emit("increment")
+document.querySelector('#message-form').addEventListener('submit', (e)=>{
+    e.preventDefault()
+
+    // const message = document.querySelector('input').value
+    const message = e.target.elements.message.value
+    socket.emit('sendMessage', message, (error)=>{
+        if(error){
+           return console.log(error);
+        }
+    console.log("Message Delivered");}  )
+} )
+ 
+document.querySelector('#send-location').addEventListener('click', (e)=>{
+    e.preventDefault()
+    if(!navigator.geolocation){
+        return alert("GeoLocation is not supported by your browser")
+    }
+
+    navigator.geolocation.getCurrentPosition((position)=>{
+        // console.log(position);
+        socket.emit('sendLocation', {
+            latitude:position.coords.latitude,
+            longitude: position.coords.longitude
+        }, ()=>{
+            console.log("Location Shared");
+        })
+    })
 })
+
+// socket.on('countUpdated',(count)=>{
+//     console.log("The count has been updated",count);
+// })
+
+// document.querySelector('#incrementButton').addEventListener('click', ()=>{
+//     socket.emit("increment")
+// })
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const path = require('path')
 const socketio = require('socket.io')
 const publicDirectoryPath = path.join(__dirname, '../public')
+const Filter = require("bad-words")
 
 const port = process.env.PORT || 3000
 
@@ -15,15 +16,39 @@ const io = socketio(server)
 
 let count=0
 
-io.on('connection', (socket)=>{
+//io.on --> for connection
+//socket.on ---> for disconnection
+io.on('connection', (socket)=>{  
     console.log("New Web Socket connection");
 
-    socket.emit("countUpdated", count)
+    // socket.emit("countUpdated", count)
 
-    socket.on("increment", ()=>{
-        count++
-        // socket.emit("countUpdated", count)  -----> this emits to one client
-        io.emit("countUpdated", count)
+    // socket.on("increment", ()=>{
+    //     count++
+    //     /* socket.emit("countUpdated", count)  -----> this emits to one client*/
+    //     io.emit("countUpdated", count)
+    // })
+
+    socket.emit("message", "Welcome")
+
+    socket.broadcast.emit('message', "A new user has joined")
+
+    socket.on("sendMessage", (message, callBack)=>{
+        const filter = new Filter()
+        if(filter.isProfane(message)){
+            return callBack("Profanity is not allowed")
+        }
+        io.emit('message', message)
+        callBack()
+    })  
+ 
+    socket.on('disconnect',()=>{
+        io.emit("message","A user has left")
+    })
+
+    socket.on('sendLocation', (coords, callback)=>{
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
     })
 
 })
